e61e62acbd58acf2fd1e4ad908cff3777fb4bfbd
NguyenVanLoc2002 <nguyenvanloc6756@gmail.com>
2023-09-09 11:16:31 +0700

    create project, structure.

---
+++ src/main/java/vn/edu/iuh/fit/week1_lab_nguyenvanloc_20045691/Connect/Connect.java
@@ -0,0 +1,32 @@
+package vn.edu.iuh.fit.week1_lab_nguyenvanloc_20045691.Connect;
+
+import java.sql.*;
+
+
+public class Connect {
+    private static Connection connection;
+
+    static {
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+            String url = "jdbc:mariadb://localhost:3306/mydb?createDatabaseIfNotExist=true";
+            connection = DriverManager.getConnection(url);
+        }catch (ClassNotFoundException| SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() {
+        return connection;
+    }
+
+    public static void closeConnection(){
+        if(connection != null){
+            try {
+                connection.close();
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- src/main/java/vn/edu/iuh/fit/week1_lab_nguyenvanloc_20045691/reponsitories/AccountReponsitory.java
+++ src/main/java/vn/edu/iuh/fit/week1_lab_nguyenvanloc_20045691/reponsitories/AccountReponsitory.java
@@ -1,4 +1,24 @@
 package vn.edu.iuh.fit.week1_lab_nguyenvanloc_20045691.reponsitories;
 
+import vn.edu.iuh.fit.week1_lab_nguyenvanloc_20045691.Connect.Connect;
+import vn.edu.iuh.fit.week1_lab_nguyenvanloc_20045691.models.Account;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.List;
+import java.util.Optional;
+
 public class AccountReponsitory {
+    private Connection connection;
+
+    public AccountReponsitory() {
+        connection = Connect.getConnection();
+    }
+
+    public Optional<Account> getAll() throws Exception{
+        String sql = "select * from account";
+        PreparedStatement ps = connection.prepareStatement(sql);
+        
+
+    }
 }
